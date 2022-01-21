23a09d65120554bab02484a2731f715b404dd23c
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-01-21 14:12:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'; 
-import Button from './components/Button'
-import {useState} from 'react' 
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import About from './Pages/About/About'; 
 import Home from './Pages/Home/Home'; 
 import Portfolio from './Pages/Portfolio/Portfolio'; 
-
-// import ParticlesBg from 'particles-bg'
-
 
 function App() {
   return (
@@ -26,18 +21,5 @@ function App() {
   );
 }
 
-/* "color"
-"ball"
-"lines"
-"thick"
-"circle"
-"cobweb"
-"polygon"
-"square"
-"tadpole"
-"fountain"
-"random"
-"custom"
-*/ 
 
 export default App;
