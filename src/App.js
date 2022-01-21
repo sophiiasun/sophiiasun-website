d9bc558a7732754add37cd38d6f26685fbc5f3f4
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-01-21 17:49:59 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar'; 
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
-import About from './Pages/About/About'; 
-import Home from './Pages/Home/Home'; 
-import Portfolio from './Pages/Portfolio/Portfolio'; 
+// import Navbar from './components/Navbar/Navbar'; 
+// import './App.css';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+// import About from './Pages/About/About'; 
+// import Home from './Pages/Home/Home'; 
+// import Portfolio from './Pages/Portfolio/Portfolio'; 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Navbar /> 
         <Switch>
           <Route path = '/sophiiasun-website' exact component = {Home}/>
           <Route path = '/About' component = {About}/>
           <Route path = '/Portfolio' component = {Portfolio}/>
         </Switch>
-      </Router>
+      </Router> */}
     </div>  
   );
 }
