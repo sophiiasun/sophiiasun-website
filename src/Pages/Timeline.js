1db2c03a6e7f82bef383b43fa0f015c4dadafc6b
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-02-04 10:21:24 -0500

    c

--- src/Pages/Timeline.js
+++ src/Pages/Timeline.js
@@ -6,7 +6,7 @@ import TimelineCard from '../components/Card/TimelineCard'
 const Timeline = () => {
 	return (
 		<>
-			<div class="timelineContainer">
+			{/* <div class="timelineContainer">
 				<div class="timeline">
                 <div class="icon"></div>
                 <div class="date-content">
@@ -22,7 +22,7 @@ const Timeline = () => {
                     <p class="description">description</p>
                 </div>
             </div>
-			</div>
+			</div> */}
 		</>
 	)
 }
--- src/components/Card/TimelineCard.js
+++ src/components/Card/TimelineCard.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import '../../components/Components.css'
 
-const TimelineCard = ({length, year, title, description}) => {
-	return (
-		<>
-			<div class="timeline">
-                <div class="icon"></div>
-                <div class="date-content">
-                    <div class="date-outer">
-                        <span class="date">
-                            <span class="month">{length}</span>
-                            <span class="year">{year}</span>
-                        </span>
-                    </div>
-                </div>
-                <div class="timeline-content">
-                    <h5 class="timelineTitle">{title}</h5>
-                    <p class="description">{description}</p>
-                </div>
-            </div>
-		</>
-	)
-}
+// const TimelineCard = ({length, year, title, description}) => {
+// 	return (
+// 		<>
+// 			<div class="timeline">
+//                 <div class="icon"></div>
+//                 <div class="date-content">
+//                     <div class="date-outer">
+//                         <span class="date">
+//                             <span class="month">{length}</span>
+//                             <span class="year">{year}</span>
+//                         </span>
+//                     </div>
+//                 </div>
+//                 <div class="timeline-content">
+//                     <h5 class="timelineTitle">{title}</h5>
+//                     <p class="description">{description}</p>
+//                 </div>
+//             </div>
+// 		</>
+// 	)
+// }
 
-export default TimelineCard
+// export default TimelineCard
