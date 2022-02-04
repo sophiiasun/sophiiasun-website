c9fdf79f1d3b4440526a499af2b6b884c4f5be0a
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-02-04 10:13:17 -0500

    commit

--- src/Pages/Timeline.js
+++ src/Pages/Timeline.js
@@ -1,139 +1,27 @@
 import React from 'react'
 import ParticlesBg from 'particles-bg'
 import './Pages.css'
+import TimelineCard from '../components/Card/TimelineCard'
 
 const Timeline = () => {
 	return (
 		<>
 			<div class="timelineContainer">
-				<div class="main-timeline">
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2013</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Visual Art &amp; Design</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">1 Years</span>
-									<span class="year">2015</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle" style={{color:'var(--col1)'}}>Product Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2016</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Web Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2018</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Graphic Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2018</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Graphic Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2018</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Graphic Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-					<div class="timeline">
-						<div class="icon"></div>
-						<div class="date-content">
-							<div class="date-outer">
-								<span class="date">
-									<span class="month">2 Years</span>
-									<span class="year">2018</span>
-								</span>
-							</div>
-						</div>
-						<div class="timeline-content">
-							<h5 class="timelineTitle">Graphic Designer</h5>
-							<p class="description">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-							</p>
-						</div>
-					</div>
-
-				</div>
+				<div class="timeline">
+                <div class="icon"></div>
+                <div class="date-content">
+                    <div class="date-outer">
+                        <span class="date">
+                            <span class="month">length</span>
+                            <span class="year">year</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="timeline-content">
+                    <h5 class="timelineTitle">title</h5>
+                    <p class="description">description</p>
+                </div>
+            </div>
 			</div>
 		</>
 	)
--- src/components/Card/TimelineCard.js
+++ src/components/Card/TimelineCard.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../components/Components.css'
 
-const Card = ({ header, body, imgLink, date, github, devpost }) => {
+const TimelineCard = ({length, year, title, description}) => {
 	return (
 		<>
 			<div class="timeline">
@@ -9,20 +9,18 @@ const Card = ({ header, body, imgLink, date, github, devpost }) => {
                 <div class="date-content">
                     <div class="date-outer">
                         <span class="date">
-                            <span class="month">1 Years</span>
-                            <span class="year">2015</span>
+                            <span class="month">{length}</span>
+                            <span class="year">{year}</span>
                         </span>
                     </div>
                 </div>
                 <div class="timeline-content">
-                    <h5 class="timelineTitle">Product Designer</h5>
-                    <p class="description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-                    </p>
+                    <h5 class="timelineTitle">{title}</h5>
+                    <p class="description">{description}</p>
                 </div>
             </div>
 		</>
 	)
 }
 
-export default Card
+export default TimelineCard
