cbd055866507f9004b363f934b002fd3dba8b01d
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-02-07 23:35:35 -0500

    image effect done :D

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -8,11 +8,28 @@ import FlipCard from '../components/Card/FlipCard'
 const test = () => {
 	return (
 		<>
-            <div class="container3">
+            <div class="gridContainer">
                 <div
-                    class="container3__image">
-                    <div class="container3__info">Photo by <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">Silvia Diaconescu</a></div>
-                    <div class="container3__info container3__location">Geneva Lake Switzerland </div>
+                    class="containerR__image">
+                    <div class="containerR__info">Photo by <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">Silvia Diaconescu</a></div>
+                    <div class="containerR__info containerR__location">Geneva Lake Switzerland </div>
+                </div>
+                <div
+                    class="containerL__image">
+                    <div class="containerL__info">Photo by <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">Silvia Diaconescu</a></div>
+                    <div class="containerL__info containerL__location">Geneva Lake Switzerland </div>
+                </div>
+            </div>
+            <div class="gridContainer">
+                <div
+                    class="containerR__image">
+                    <div class="containerR__info">Photo by <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">Silvia Diaconescu</a></div>
+                    <div class="containerR__info containerR__location">Geneva Lake Switzerland </div>
+                </div>
+                <div
+                    class="containerL__image">
+                    <div class="containerL__info">Photo by <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">Silvia Diaconescu</a></div>
+                    <div class="containerL__info containerL__location">Geneva Lake Switzerland </div>
                 </div>
             </div>
         </>
